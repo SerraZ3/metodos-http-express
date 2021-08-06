57dafff7aa7fc35043994b79899cdb36900c54c9
SerraZ3 <serra.henrique3@gmail.com>
2021-08-05 23:19:45 -0300

    adicionando comentários

--- app.js
+++ app.js
@@ -4,21 +4,37 @@ const port = 3000;
 
 var listaFrutas = [];
 
-app.get("/frutas", (req, res) => {
-  res.json({ frutas: listaFrutas });
-});
+// GET -> Retorna dados ou interface. Faz pesquisas.
+// POST -> Salvar dados. Cadastros
+// PUT/PATCH -> Atualizar dados
+// DELETE -> Deletar dados
+
+// CRUD
+// Create -> Criar -> POST
+// Read -> Ler/Visualizar -> Get
+// Update -> Atualizar -> PUT/PATCH
+// Delete -> Deletar -> DELETE
+
+// Create -> Criar
 app.post("/frutas", (req, res) => {
   listaFrutas.push("Banana");
   res.json({ frutas: listaFrutas });
 });
+// Read -> Ler
+app.get("/frutas", (req, res) => {
+  res.json({ frutas: listaFrutas });
+});
+// Update -> Atualizar
 app.put("/frutas", (req, res) => {
   listaFrutas[0] = "Pera";
   res.json({ frutas: listaFrutas });
 });
+// Update -> Atualizar
 app.patch("/frutas", (req, res) => {
   listaFrutas[0] = "Limão";
   res.json({ frutas: listaFrutas });
 });
+// Delete -> Deletar
 app.delete("/frutas", (req, res) => {
   listaFrutas.pop();
   res.json({ frutas: listaFrutas });
